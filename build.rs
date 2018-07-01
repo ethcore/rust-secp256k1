cf1d395f1b8c15493f80dd53a8d3dd115a1c6009
G. Baecker <admin@o34.mooo.com>
2018-07-01 18:02:12 +0200

    Added `OS` string to build on FreeBSD

--- build.rs
+++ build.rs
@@ -29,6 +29,8 @@ use std::path::PathBuf;
 
 #[cfg(target_os = "macos")]
 const OS: &'static str = "darwin";
+#[cfg(target_os = "freebsd")]
+const OS: &'static str = "freebsd";
 #[cfg(target_os = "linux")]
 const OS: &'static str = "linux";
 #[cfg(target_os = "windows")]
